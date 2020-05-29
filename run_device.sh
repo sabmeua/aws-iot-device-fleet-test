09b7539f14c1babd51586b3692717810a4a69ad7
sabmeua <sabme.ua@gmail.com>
2020-05-30 07:26:14 +0900

    Add device shadow test script

--- run_device.sh
+++ run_device.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /aws-iot-fleet-provisioning
-ls -l certs
 cat ./config.ini
 python3 main.py
+python3 updateDeviceShadow.py
---
+++ updateDeviceShadow.py
@@ -0,0 +1,40 @@
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
+import time
+import json
+import random
+
+def callback(payload, status, token):
+    if status == "timeout":
+        print(f'Timeout: {token}')
+    if status == "accepted":
+        payloadDict = json.loads(payload)
+        print(f'Accepted: {token}')
+        print(payload)
+    if status == "rejected":
+        print("Rejected: " + token)
+
+with open('device_info.json') as f:
+    dev = json.load(f)
+
+shadowClient = AWSIoTMQTTShadowClient(dev['thingName'])
+shadowClient.configureEndpoint(dev['endpoint'], 8883)
+shadowClient.configureCredentials(
+    f'{dev["certPath"]}/{dev["rootCert"]}',
+    f'{dev["certPath"]}/{dev["privateKey"]}',
+    f'{dev["certPath"]}/{dev["cert"]}')
+shadowClient.configureAutoReconnectBackoffTime(1, 32, 20)
+shadowClient.configureConnectDisconnectTimeout(10)
+shadowClient.configureMQTTOperationTimeout(5)
+
+shadowClient.connect()
+shadowHandler = shadowClient.createShadowHandlerWithName(dev['thingName'], True)
+shadowHandler.shadowDelete(callback, 5)
+
+prop = {'state':{'desired':{'batt': 100.0}}}
+while True:
+    batt = random.betavariate(6, 3) * 100
+    prop['state']['desired']['batt'] = batt
+    shadowHandler.shadowUpdate(json.dumps(prop), callback, 5)
+    if batt < 50:
+        break
+    time.sleep(30)
