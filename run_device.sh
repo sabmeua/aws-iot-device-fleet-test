bae218d474e0ec28c13544734f4156026b7ee633
sabmeua <sabme.ua@gmail.com>
2020-05-31 10:26:52 +0900

    Add random wait

--- run_device.sh
+++ run_device.sh
@@ -2,6 +2,9 @@
 
 cd /aws-iot-fleet-provisioning
 if [ ! -f ./device_info.json ]; then
+  usleep $(expr $RANDOM \* $RANDOM % 10000000)
   python3 main.py
 fi
-python3 -u updateDeviceShadow.py
+if [ -f ./device_info.json ]; then
+  python3 -u updateDeviceShadow.py
+fi
